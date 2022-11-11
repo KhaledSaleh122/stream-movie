42ca74795a45587e8aa4ad014cdf83e02f4a3786
Khaled Saleh <71279851+KhaledSaleh122@users.noreply.github.com>
2022-11-11 20:25:39 +0200

    as

---
+++ index.js
@@ -0,0 +1,29 @@
+//require packages
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended :true}));
+app.use(express.urlencoded({ extended: false }));
+//const https = require('https');
+const { http, https } = require('follow-redirects');
+//
+var port = 8080;
+app.listen(port,function(){
+    console.log("Server Started at port "+ port);
+});
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");//sending movie name to search using <form action="/search" method="get"> ->> input name "q"
+});
+
+app.get("/search",function(req,res){
+    var mName = req.query.q;
+    console.log("Movie Name : "+mName);
+    var qUrl = "https://shahed4u.vip/";
+    const qUrl_O = new URL(qUrl);
+    console.log("Domain of website "+ qUrl);
+    const request = https.request({ host: 'shahed4u.vip', path: '/'}, response => {
+        console.log(response.responseUrl);
+    });
+    request.end();
+});
